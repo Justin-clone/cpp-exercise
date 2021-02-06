e42ab55e91b23f0a6a346694cac92290014a672b
Justin-clone <73817356+Justin-clone@users.noreply.github.com>
2021-02-06 11:44:29 -0800

    Exerc1.cpp

---
+++ Ex 1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+  char i;
+  int num;
+  cout << "Please enter an character: ";
+  cin >> i;
+  cout<< "Please enter a number: ";
+  cin >> num;
+  cout << "You entered the character: " << i <<"\n";
+  cout << "You entered the number: " << num;
+  return 0;
+}
