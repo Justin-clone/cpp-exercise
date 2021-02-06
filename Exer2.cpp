57fa8d8ac5b66ec94fb16f163835be1b321cef7a
Justin-clone <73817356+Justin-clone@users.noreply.github.com>
2021-02-06 11:54:51 -0800

    Add files via upload

---
+++ Exer2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+int num1, num2;
+
+
+cout<<"Input first number: ";
+cin>> num1;
+cout<<"Input second number: ";
+cin>> num2;
+cout<<"Sum: "<<num1 + num2;
+
+  return 0;
+}
