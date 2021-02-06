48c86dbc689ea815abd215357e754d2acd24f102
Justin-clone <73817356+Justin-clone@users.noreply.github.com>
2021-02-06 12:13:36 -0800

    Add files via upload

---
+++ Exer4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+int length, width;
+
+cout<<"Enter length: ";
+cin>>length;
+cout<<"Enter width: ";
+cin>>width;
+
+cout<<"The perimiter is: "<<(width + length) * 2;
+
+  return 0;
+}
