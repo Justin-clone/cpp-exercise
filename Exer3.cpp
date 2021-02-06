025d3797fca220fd1fd0db07d770611df2ac4feb
Justin-clone <73817356+Justin-clone@users.noreply.github.com>
2021-02-06 12:05:44 -0800

    Add files via upload

---
+++ Exer3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+
+int num1, num2;
+
+
+cout<<"Input first number: ";
+cin>> num1;
+cout<<"Input second number: ";
+cin>> num2;
+cout<<"Sum: "<<num1 + num2<<"\n";
+cout<<"Difference: "<<num1 - num2<<"\n";
+cout<<"Product: "<<num1 * num2<<"\n";
+cout<<"Quotient: "<<num1/num2<<"\n";
+cout<<"Modulus: "<<num1%num2;
+
+  return 0;
+}
